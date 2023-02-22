12e86321449fd7f115b82cbc5e0227c59f903f0d
pjotrvp <74866171+pjotrvp@users.noreply.github.com>
2023-02-22 22:02:04 +0100

    Supplement styling

--- apps/frontend/src/app/Models/supplement.model.ts
+++ apps/frontend/src/app/Models/supplement.model.ts
@@ -1,42 +1,41 @@
 export enum SupplementType {
-    Vitamin = "vitamin",
-    FishOil = 'fishOil',
-    Protein = 'protein',
-    Creatine = "creatine",
-    Other = 'other',
-    PreWorkout =  'preWorkout',
+  Vitamin = 'Vitamin',
+  FishOil = 'Fish oil',
+  Protein = 'Protein',
+  Creatine = 'Creatine',
+  Other = 'Other',
+  PreWorkout = 'Pre-Workout',
 }
 import { Product } from './product.model';
 import { Review } from './review.model';
 export class Supplement extends Product {
-    flavours : string[];
-    sizes : string[];
-    ingredients : string[];
-    supplementType : SupplementType = SupplementType.Other;
-    containsLactose : boolean = false; 
-    isVegan : boolean = false;
-    price : number = 0;
-    constructor(
-        flavours : string[],
-        sizes : string[],
-        id : string,
-        name : string,
-        supplementType : SupplementType,
-        containsLactose : boolean,
-        isVegan : boolean,
-        price : number,
-        reviews : Review[],
-        ingredients : string[],
-    ) {
-        super(id, name, reviews);
-        this.flavours = flavours;
-        this.sizes = sizes;
-        this.name = name;
-        this.supplementType = supplementType;
-        this.containsLactose = containsLactose;
-        this.isVegan = isVegan;
-        this.price = price;
-        this.ingredients = ingredients;
-     }
-        
-    }
+  flavours: string[];
+  sizes: string[];
+  ingredients: string[];
+  supplementType: SupplementType = SupplementType.Other;
+  containsLactose: boolean = false;
+  isVegan: boolean = false;
+  price: number = 0;
+  constructor(
+    flavours: string[],
+    sizes: string[],
+    id: string,
+    name: string,
+    supplementType: SupplementType,
+    containsLactose: boolean,
+    isVegan: boolean,
+    price: number,
+    reviews: Review[],
+    ingredients: string[]
+  ) {
+    super(id, name, reviews);
+    this.flavours = flavours;
+    this.sizes = sizes;
+    this.name = name;
+    this.supplementType = supplementType;
+    this.containsLactose = containsLactose;
+    this.isVegan = isVegan;
+    this.price = price;
+    this.ingredients = ingredients;
+  }
+}

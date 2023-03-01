b64b851d739737ae31fa982de8a4b89162971631
pjotrvp <74866171+pjotrvp@users.noreply.github.com>
2023-03-01 14:42:53 +0100

    user reference

--- apps/api/src/app/domain/product/product.schema.ts
+++ apps/api/src/app/domain/product/product.schema.ts
@@ -2,7 +2,7 @@ import { Review } from '../review/review.schema';
 import { Order } from '../order/order.schema';
 import mongoose, { HydratedDocument } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-
+import { User } from '../user/user.schema';
 
 export type ProductDocument = HydratedDocument<Product>;
 @Schema()
@@ -24,6 +24,9 @@ export class Product {
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Order' })
   orders: Order;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  createdBy: User;
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);

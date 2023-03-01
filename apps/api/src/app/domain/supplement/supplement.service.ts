e09836b69eaf32821049547b44a410d7f6137ffa
pjotrvp <74866171+pjotrvp@users.noreply.github.com>
2023-03-01 14:43:06 +0100

    neo4j queries

--- apps/api/src/app/domain/supplement/supplement.service.ts
+++ apps/api/src/app/domain/supplement/supplement.service.ts
@@ -4,16 +4,22 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateSupplementDto } from './dto/createSupplement.dto';
 import { UpdateSupplementDto } from './dto/updateSupplement.dto';
+import { Neo4jService } from '../../Infrastructure/neo4j/neo4j.service';
+
 @Injectable()
 export class SupplementService {
   constructor(
     @InjectModel(Supplement.name)
-    private supplementModel: Model<SupplementDocument>
+    private supplementModel: Model<SupplementDocument>,
+    private readonly neo4jService: Neo4jService,
   ) {
     console.log('SupplementService: ', Supplement);
   }
   async create(supplementDto: CreateSupplementDto): Promise<Supplement> {
     const createdSupplement = new this.supplementModel(supplementDto);
+    await this.neo4jService.write(
+      `CREATE (s:Supplement {name: "${createdSupplement.name}", createdBy: "${createdSupplement.createdBy}", id: "${createdSupplement._id}"}) MERGE (u:User {id: "${createdSupplement.createdBy}"}) MERGE (u)-[:CREATED]->(s) RETURN s`
+    );
     return createdSupplement.save();
   }
 
@@ -31,7 +37,20 @@ export class SupplementService {
     return this.supplementModel.findById(id).exec();
   }
 
+  async findRecommendations(id: string): Promise<Supplement[]> {
+    const result = await this.neo4jService.read(
+      `MATCH (s:Supplement {id: "${id}"})-[:RECOMMENDED]->(r:Supplement) RETURN r`
+    );
+    const recommendations = result.records.map((record) => {
+      return record.get('r').properties;
+    });
+    return recommendations;
+  }
+
   async remove(id: string) {
+    await this.neo4jService.write(
+      `MATCH (s:Supplement {id: "${id}"}) DETACH DELETE s`
+    )
     return this.supplementModel.findByIdAndRemove(id).exec();
   }
 }

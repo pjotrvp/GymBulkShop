a16792c9b9b8cde94d3b3ec511e57a4632d80db9
pjotrvp <74866171+pjotrvp@users.noreply.github.com>
2023-03-06 16:12:11 +0100

    new neo4j queries

--- apps/api/src/app/domain/user/user.module.ts
+++ apps/api/src/app/domain/user/user.module.ts
@@ -3,9 +3,10 @@ import { User, UserSchema } from './user.schema';
 import { UserService} from './user.service';
 import { UserController} from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Neo4jModule } from '../../Infrastructure/neo4j/neo4j.module';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+    imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), Neo4jModule],
     providers: [UserService],
     controllers: [UserController],
 })
--- apps/api/src/app/domain/user/user.service.ts
+++ apps/api/src/app/domain/user/user.service.ts
@@ -5,20 +5,28 @@ import { CreateUserDto } from './dto/createUser.dto';
 import { UpdateUserDto } from './dto/updateUser.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-
+import { Neo4jService } from '../../Infrastructure/neo4j/neo4j.service';
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private readonly neo4jService: Neo4jService
+  ) {
     console.log('UserService: ', User);
   }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     const createdUser = new this.userModel(createUserDto);
+    this.neo4jService.write(
+      `CREATE (u:User {name: "${createdUser.name}", id: "${createdUser._id}"}) RETURN u`
+    );
     return createdUser.save();
   }
 
   async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
-    return this.userModel.findByIdAndUpdate({ _id: id}, updateUserDto, { new: true }).exec();
+    return this.userModel
+      .findByIdAndUpdate({ _id: id }, updateUserDto, { new: true })
+      .exec();
   }
 
   async findAll(): Promise<User[]> {
@@ -26,13 +34,16 @@ export class UserService {
   }
 
   async findOne(id: string): Promise<User> {
-    return this.userModel.findOne({ _id: id}).exec();
+    return this.userModel.findOne({ _id: id }).exec();
   }
 
   async delete(id: string) {
+    await this.neo4jService.write(
+      `MATCH (u:User {id: "${id}"}) DETACH DELETE u`
+    );
     const deletedUser = await this.userModel
-    .findByIdAndRemove({ _id: id})
-    .exec();
+      .findByIdAndRemove({ _id: id })
+      .exec();
     return deletedUser;
   }
 }

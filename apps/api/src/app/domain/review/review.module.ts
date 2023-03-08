5a904f3c64cda10d0d6ee238ff6dde0f9a33c977
pjotrvp <74866171+pjotrvp@users.noreply.github.com>
2023-03-08 14:05:49 +0100

    Review neo queries on CREATE + DELETE

--- apps/api/src/app/domain/review/review.module.ts
+++ apps/api/src/app/domain/review/review.module.ts
@@ -3,8 +3,9 @@ import { Review, ReviewSchema } from './review.schema';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Neo4jModule } from 'nest-neo4j/dist';
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Review.name, schema: ReviewSchema }])],
+    imports: [MongooseModule.forFeature([{ name: Review.name, schema: ReviewSchema }]), Neo4jModule],
     providers: [ReviewService],
     controllers: [ReviewController],
 })

6a1cae1e3f52a13d52547f9fb5b4c73122c7fe2d
pjotrvp <74866171+pjotrvp@users.noreply.github.com>
2023-03-25 13:17:54 +0100

    wrote test for correct register call

--- apps/api/src/app/auth/auth.controller.spec.ts
+++ apps/api/src/app/auth/auth.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserDto } from '../domain/user/dto/createUser.dto';
 import { UserService } from '../domain/user/user.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -30,6 +31,35 @@ describe('AuthController', () => {
     authController = app.get<AuthController>(AuthController);
     authService = app.get<AuthService>(AuthService);
     userService = app.get<UserService>(UserService);
+  });
+
+  describe('register', () => {
+    let exampleUser, exampleId, register;
+
+    beforeEach(() => {
+      exampleUser = {
+        username: 'Bart',
+        password: 'supergeheim124',
+        email: 'Bart@proton.me',
+      };
+      exampleId = 'id123';
+    });
+
+    it('should call the register and create method of the auth service on success', async () => {
+      register = jest
+        .spyOn(userService, 'create')
+        .mockImplementation(async (_user: CreateUserDto) => {
+          exampleUser._id = exampleId;
+          return exampleUser;
+        });
+
+      const user = await authController.register(exampleUser);
+
+      expect(register).toHaveBeenCalledWith(exampleUser);
+      expect(user).toHaveProperty('_id', exampleId);
+    });
+
+    
   });
   it('should be defined', () => {
     expect(authController).toBeDefined();

38f67ebcfc8aa8b0452c936f6f9fb57ed04bc7c4
pjotrvp <74866171+pjotrvp@users.noreply.github.com>
2023-04-04 12:41:22 +0200

    removed review controller

--- apps/api/src/app/domain/review/review.module.ts
+++ apps/api/src/app/domain/review/review.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { Review, ReviewSchema } from './review.schema';
 import { ReviewService } from './review.service';
-import { ReviewController } from './review.controller';
+
 import { MongooseModule } from '@nestjs/mongoose';
 import { Neo4jModule } from '../../Infrastructure/neo4j/neo4j.module';
 @Module({
@@ -10,6 +10,6 @@ import { Neo4jModule } from '../../Infrastructure/neo4j/neo4j.module';
     Neo4jModule,
   ],
   providers: [ReviewService],
-  controllers: [ReviewController],
+  
 })
 export class ReviewModule {}

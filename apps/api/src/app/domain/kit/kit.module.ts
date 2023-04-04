833daa19ab90d07101c33ef8ef6d54dd7e9da9a4
pjotrvp <74866171+pjotrvp@users.noreply.github.com>
2023-04-04 12:41:10 +0200

    removed unused kit controller

--- apps/api/src/app/domain/kit/kit.module.ts
+++ apps/api/src/app/domain/kit/kit.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { Kit, KitSchema } from './kit.schema';
 import { KitService } from './kit.service';
-import { KitController } from './kit.controller';
+
 import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Kit.name, schema: KitSchema }])],
-    providers: [KitService],
-    controllers: [KitController],
+  imports: [MongooseModule.forFeature([{ name: Kit.name, schema: KitSchema }])],
+  providers: [KitService],
 })
-
 export class KitModule {}

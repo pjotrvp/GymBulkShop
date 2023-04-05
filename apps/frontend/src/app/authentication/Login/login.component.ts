26e0b6698b194327c9979055f0b646b4880d6566
pjotrvp <74866171+pjotrvp@users.noreply.github.com>
2023-04-05 19:44:57 +0200

    Expanded and actual functional authorization

--- apps/frontend/src/app/authentication/Login/login.component.ts
+++ apps/frontend/src/app/authentication/Login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from '../../Models/user.model';
+import { AuthService } from '../Services/auth.service';
 
 @Component({
   selector: 'gym-bulk-shop-login',
@@ -7,13 +10,37 @@ import { Router } from '@angular/router';
   styleUrls: [],
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router) {}
+  subscription!: Subscription;
+  user: any = {};
+
+  constructor(private router: Router, private authservice: AuthService) {}
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.user = new User('', '', '', '');
+    this.subscription = this.authservice
+      .getUserFromLocalStorage()
+      .subscribe((user) => {
+        if (user) {
+          this.router.navigate(['/']);
+        }
+      });
   }
 
   login() {
-    this.router.navigate(['/home']);
+    this.subscription = this.authservice
+      .getUserByEmail(this.user.email)
+      .subscribe({
+        next: (user) => {
+          if (user) {
+            if (user.email === this.user.email) {
+              this.authservice.login(this.user.email, this.user.password);
+            } else {
+              window.alert('Invalid credentials');
+            }
+          } else {
+            window.alert('User not found!');
+          }
+        },
+      });
   }
 }
--- apps/frontend/src/app/authentication/Register/register.component.ts
+++ apps/frontend/src/app/authentication/Register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../Services/auth.service';
 
 @Component({
   selector: 'gym-bulk-shop-register',
@@ -7,12 +9,36 @@ import { Router } from '@angular/router';
   styleUrls: [],
 })
 export class RegisterComponent implements OnInit {
-  constructor(private router: Router) {}
+  user: any = {};
+  subscription!: Subscription;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authservice: AuthService
+  ) {}
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.user = {
+      email: '',
+      password: '',
+      name: '',
+    };
+    this.subscription = this.authservice
+      .getUserFromLocalStorage()
+      .subscribe((user) => {
+        if (user) {
+          this.router.navigate(['/']);
+        }
+      });
   }
 
   register() {
-    this.router.navigate(['/login']);
+    this.authservice.register(this.user).subscribe({
+      next: (user) => {
+        if (user) {
+          this.authservice.login(this.user.email, this.user.password);
+        }
+      },
+    });
   }
 }
